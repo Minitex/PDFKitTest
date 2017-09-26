2b74e8ed4f55a2c81882a2005f4f6d3eccaf46ec
Vui Nguyen <vui_engineer@yahoo.com>
2017-09-26 11:35:53 -0600

    got initial thumbnails to display

--- PDFKitTest/ThumbnailViewController.swift
+++ PDFKitTest/ThumbnailViewController.swift
@@ -12,13 +12,17 @@ import PDFKit
 class ThumbnailViewController: UIViewController, PDFDocumentDelegate {
 
   var document: PDFDocument?
-
-  @IBOutlet weak var pdfView: PDFView!
+  var pdfView: PDFView?
+  var pdfThumbnailView: PDFThumbnailView?
+  
+  @IBOutlet weak var uiView: UIView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-
+    //displayThumbnails()
+    //displayPDF()
+    displayThumbnails()
 
 
   }
@@ -29,20 +33,31 @@ class ThumbnailViewController: UIViewController, PDFDocumentDelegate {
   }
 
   func displayThumbnails() {
-
+    let thumbnailRect = CGRect(x: 0, y: Int(self.uiView.frame.height) - 150, width: Int(self.uiView.frame.width), height: 150)
+    pdfThumbnailView = PDFThumbnailView(frame: thumbnailRect)
+    pdfThumbnailView?.thumbnailSize = CGSize(width: 25, height: 50)
+    displayPDF()
+    pdfThumbnailView?.pdfView = pdfView
+    pdfThumbnailView?.layoutMode = .horizontal
+    pdfView?.addSubview(pdfThumbnailView!)
   }
 
   func displayPDF() {
 
     if let document = document {
-      pdfView.backgroundColor = UIColor.lightGray.withAlphaComponent(0.25)
-      pdfView.autoScales = true
+      let pdfRect = CGRect(x: 0, y: 0, width: Int(self.uiView.frame.width), height:Int(self.uiView.frame.height) - 150)
+      pdfView = PDFView(frame: pdfRect)
+      pdfView?.backgroundColor = UIColor.lightGray.withAlphaComponent(0.25)
+      pdfView?.autoScales = true
       pdfView?.usePageViewController(true, withViewOptions: nil)
 
       // 1. Set delegate
       document.delegate = self
       pdfView?.document = document
       //pdfView.displayDirection = .horizontal
+
+      self.uiView.addSubview(pdfView!)
+      //self.view = pdfView
       print("set document success!")
       } else {
       print("set document NOT successful!")
